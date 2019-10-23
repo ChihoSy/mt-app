86c5b5e52344e9696abfbf028402b5b7f4bf5b36
Chiho <yyflying11d@163.com>
2019-10-23 10:55:19 +0800

    提交首页header

---
+++ components/public/header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <div class="topbar">
+      <top-bar/>
+    </div>
+    <div class="searchbar">
+      <search-bar/>
+    </div>
+  </div>
+</template>
+
+<script>
+import topBar from './topbar.vue'
+import searchBar from './searchbar.vue'
+export default {
+  components:{
+    topBar,
+    searchBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .searchbar{
+    background-color: #fff;
+  }
+</style>
---
+++ components/public/header/topbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-topbar">
+    <div class="m-topbar-leftbar">
+      <geo />
+      <user />
+    </div>
+    <div class="m-topbar-nav">
+      <navbar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Geo from './geo.vue'
+import User from './user.vue'
+import Navbar from './nav.vue'
+export default {
+  components: {
+    Geo,
+    User,
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.m-topbar {
+  height: 40px;
+  width: 1190px;
+  margin: 0 auto;
+}
+.m-topbar-leftbar {
+  height: 40px;
+  float: left;
+  display: flex;
+  align-items: center;
+}
+.m-topbar-nav {
+  height: 40px;
+  float: right;
+}
+</style>
